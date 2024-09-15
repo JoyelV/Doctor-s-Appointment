62fc4250331bdbc1b0f3da5070e611fb34e555dc
Joyel <Varghesejoyel71@gmail.com>
2024-09-15 17:38:15 +0530

    second commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import {Routes,Route} from 'react-router-dom'
 
 const App = () => {
   return (
     <div>
-      <div className='text-green 500'>
-      Testing Tialwind
+      <div className='mx-4 sm:mx=[10%]'>
+       <Routes>
+        <Route path='/' element={<Home/>}>
+
+        </Route>
+       </Routes>
       </div>
     </div>
   )
--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-function Home() {
+function login() {
   return (
     <div>
-      Home Page
+      
     </div>
   )
 }
 
-export default Home
+export default login
