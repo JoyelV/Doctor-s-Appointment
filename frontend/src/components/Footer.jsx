2b762db61137c7326519fa6c769e6aecceb22ca4
Joyel <Varghesejoyel71@gmail.com>
2024-09-21 20:50:22 +0530

    Fourth commit on day 21st sep 2024

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,15 +3,15 @@ import { assets } from '../assets/assets_frontend/assets'
 
 const Footer = () => {
   return (
-    <div>
-      <div>
+    <div className='md:mx-10'>
+      <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
         <div>
-           <img src={assets.logo} alt='' />
-           <p>Our Company is trusted company with years of expertise in this particular field. Our Mission to provide high quality services in the healthcare industry</p>
+           <img className='mb-5 w-40' src={assets.logo} alt='' />
+           <p className='w-full md:w-2/3 text-gray-600 leading-6'>Our Company is trusted company with years of expertise in this particular field. Our Mission to provide high quality services in the healthcare industry</p>
         </div>
       <div>
-        <p>COMPANY</p>
-        <ul>
+        <p className='text-xl font-medium mb-5'>COMPANY</p>
+        <ul className='flex flex-col gap-2 text-gray-600'>
             <li>Home</li>
             <li>About us</li>
             <li>Contact us</li>
@@ -20,18 +20,18 @@ const Footer = () => {
         </ul>
       </div>
         <div>
-          <p>GET IN TOUCH</p>
-          <ul>
-            <li>+1-212-456-7890</li>
+          <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
+          <ul className='flex flex-col gap-2 text-gray-600'>
+          <li>+1-212-456-7890</li>
             <li>varghesejoyel71@gmail.com</li>
           </ul>
+          </div>
         </div>
         {/*---- Copyright Text-----*/}
         <div>
             <hr />
-            <p>Copyright 2024@ Prescripto - All Right Reserved.</p>
+            <p className='py-5 text-sm text-center'>Copyright 2024@ Prescripto - All Right Reserved.</p>
         </div>
-      </div>
     </div>
   )
 }
